1f10e8f531be12936edfb4f1cc23bd93b2705f18
nicolau <estevao_nicolau@hotmail.com>
2022-05-25 22:30:07 -0300

    :hammer: product_list

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,7 +5,24 @@ import 'package:flutter_provider/models/product.dart';
 class ProductList with ChangeNotifier {
   List<Product> _items = dummyData;
 
-  List<Product> get items => [..._items];
+  bool _showFavoriteOnly = false;
+
+  List<Product> get items {
+    if(_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/view/products_view.dart
+++ lib/view/products_view.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_provider/models/product_list.dart';
+import 'package:provider/provider.dart';
 
 import '../components/products_grid.dart';
 
-
+enum FilterOptions {
+  Favorite,
+  All
+}
 class ProductsView extends StatelessWidget {
   ProductsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Lista de Produtos')),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente favorito'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
       body: ProductGrid(),
     );
   }
 }
-
-
