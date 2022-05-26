2f12163b3c6889e745834e1869b99836bcef1ade
nicolau <estevao_nicolau@hotmail.com>
2022-05-25 21:37:18 -0300

    :sparkles: favorite

--- lib/components/products_items.dart
+++ lib/components/products_items.dart
@@ -27,14 +27,16 @@ class ProductItems extends StatelessWidget {
           ),
           footer: GridTileBar(
               backgroundColor: Colors.black54,
-              leading: IconButton(
-                onPressed: () {
-                  product.toggleFavorite();
-                },
-                icon: Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              leading: Consumer<Product>(
+                builder: (ctx, product, _ ) => IconButton(
+                  onPressed: () {
+                    product.toggleFavorite();
+                  },
+                  icon: Icon(
+                    product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                  ),
+                  color: Theme.of(context).colorScheme.secondary,
                 ),
-                color: Theme.of(context).colorScheme.secondary,
               ),
               title: Text(
                 product.title,
